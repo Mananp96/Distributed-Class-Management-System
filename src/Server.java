0056af03f21c4f2f3be74bb1fc5172fcaf1f2e74
Akshay Shah <akshay.shah@knappily.com>
2018-06-01 15:33:54 -0400

    Managers List added to respective servers

--- src/Server.java
+++ src/Server.java
@@ -9,9 +9,6 @@ import java.io.IOException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
-/**
- * 
- */
 
 /**
  * @author Mihir
@@ -19,32 +16,62 @@ import java.rmi.registry.Registry;
  */
 public class Server {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws Exception {
-		
-			
-		CenterServerInterface mtl = new CenterServer("MTL");
-		CenterServerInterface lvl = new CenterServer("LVL");
-		CenterServerInterface ddl = new CenterServer("DDL");
+    private CenterServerInterface mtl;
+    private CenterServerInterface lvl;
+    private CenterServerInterface ddo;
 
-		addManagersToServer();
-		
-		Registry registry = LocateRegistry.createRegistry(2964);
-		registry.bind("MTL", mtl);
-		registry.bind("LVL", lvl);
-		registry.bind("DDL", ddl);
-				
-		System.out.println("Server Started");
+
+
+    /**
+     * @param args
+     */
+	public static void main(String[] args) throws Exception {
+		Server server = new Server();
+		server.runServer();
 	}
 
-    private static void addManagersToServer() throws IOException, ParseException {
+    private void runServer() throws Exception {
+
+        this.mtl = new CenterServer("MTL");
+        this.lvl = new CenterServer("LVL");
+        this.ddo = new CenterServer("DDO");
+
+        this.addManagersToServer();
+
+        Registry registry = LocateRegistry.createRegistry(2964);
+        registry.bind("MTL", this.mtl);
+        registry.bind("LVL", this.lvl);
+        registry.bind("DDL", this.ddo);
+
+        System.out.println("Server Started");
+    }
+
+    private  void addManagersToServer() throws IOException, ParseException {
         JSONParser parser = new JSONParser();
         JSONArray jsonArray = (JSONArray) parser.parse(new FileReader("resources/managerData.json"));
         for (Object object:jsonArray){
             JSONObject manager  = (JSONObject) object;
-
+            if(manager.get("managerID").toString().substring(0,3).equalsIgnoreCase("MTL")){
+                ((CenterServer) this.mtl).addManagerToList(
+                        new Manager(
+                                manager.get("managerID").toString(),
+                                manager.get("firstName").toString(),
+                                manager.get("lastName").toString()));
+            }
+            else if(manager.get("managerID").toString().substring(0,3).equalsIgnoreCase("LVL")){
+                ((CenterServer) this.lvl).addManagerToList(
+                        new Manager(
+                                manager.get("managerID").toString(),
+                                manager.get("firstName").toString(),
+                                manager.get("lastName").toString()));
+            }
+            else if(manager.get("managerID").toString().substring(0,3).equalsIgnoreCase("DDO")){
+                ((CenterServer) this.ddo).addManagerToList(
+                        new Manager(
+                                manager.get("managerID").toString(),
+                                manager.get("firstName").toString(),
+                                manager.get("lastName").toString()));
+            }
         }
     }
 
